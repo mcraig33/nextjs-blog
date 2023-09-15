083fdd4241b40418a1b9828284858223903ac724
Michael Craig <mcraig@Michaels-MacBook-Pro.local>
2023-09-15 10:47:35 -0700

    Update blog post style, layout, and apply date formatting

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,8 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -9,7 +12,6 @@ export async function getStaticProps({ params }) {
     },
   };
 }
-
 
 export async function getStaticPaths() {
     const paths = getAllPostIds();
@@ -29,12 +31,16 @@ export default function Post({ postData }) {
     } else {
         return(
             <Layout>
-                {postData.title}
-                <br />
-                {postData.id}
-                <br />
-                {postData.date}
-                <div dangerouslySetInnerHTML={{__html: postData.contentHtml}} />
+                <Head>
+                    <title>{postData.title}</title>
+                </Head>
+                <article>
+                    <h1 className={utilStyles.headingXL}>{postData.title}</h1>
+                    <div className={utilStyles.lightText}>
+                        <Date dateString={postData.date} />
+                    </div>
+                    <div dangerouslySetInnerHTML={{__html: postData.contentHtml}} />
+                </article>
             </Layout>
         )
     }
